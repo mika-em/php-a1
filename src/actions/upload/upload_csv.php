55218f80973d2a3bcc7f700dea1c1175b19be1ee
cherylau <clau136@my.bcit.ca>
2024-03-06 23:00:21 -0800

    Getting rid of commented out code

--- src/actions/upload/upload_csv.php
+++ src/actions/upload/upload_csv.php
@@ -1,10 +1,3 @@
-<!-- <form action="index.php" method="post" enctype="multipart/form-data">
-  Select CSV File to Upload:
-  <input type="file" name="fileToUpload" id="fileToUpload">
-  <input type="submit" value="Upload File" name="submit">
-</form> -->
-
-
 <?php
 ob_start();
 if (session_status() !== PHP_SESSION_ACTIVE) {
@@ -17,13 +10,12 @@ if (!isset($_SESSION['user_id'])) {
 include_once($_SERVER['DOCUMENT_ROOT'] . "/inc_header.php");
 ?>
 <div class="container mt-4"></div>
-  <form action="index.php" method="post" enctype="multipart/form-data">
-    <div class="form-group">
-      <label for="fileToUpload">Select CSV File to Upload:</label>
-      <input type="file" class="form-control-file" name="fileToUpload" id="fileToUpload" required>
-    </div>
-    <button type="submit" class="btn btn-primary" name="submit">Upload File</button>
-  </form>
+<form action="index.php" method="post" enctype="multipart/form-data">
+  <div class="form-group">
+    <label for="fileToUpload">Select CSV File to Upload:</label>
+    <input type="file" class="form-control-file" name="fileToUpload" id="fileToUpload" required>
+  </div>
+  <button type="submit" class="btn btn-primary" name="submit">Upload File</button>
+</form>
 
 <?php include_once($_SERVER['DOCUMENT_ROOT'] . "/inc_footer.php"); ?>
-
